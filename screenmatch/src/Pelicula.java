bf57c818497471f15c1971c8f73349ceb4ac44a6
INRIAS <inrialva@gmail.com>
2024-09-29 21:40:18 -0500

    One Sesion

--- screenmatch/src/Pelicula.java
+++ screenmatch/src/Pelicula.java
@@ -3,6 +3,28 @@ public class Pelicula {
     int fechaDeLanzamiento;
     int duracionEnMinutos;
     boolean incluidoEnElPlan;
+    double nota;
+    double sumaDeEvaluaciones;
+    int totalEvaluaciones;
 
+    void muestraFichaTecnica(String nombre, int fechaDeLanzamiento, int duracionEnMinutos, double nota) {
+        this.nombre = nombre;
+        this.fechaDeLanzamiento = fechaDeLanzamiento;
+        this.duracionEnMinutos = duracionEnMinutos;
+        this.nota = nota;
+
+        
+        System.out.println("El nombre de la pelicula es: " + this.nombre);
+        System.out.println("Fecha de Lanzamiento: " + this.fechaDeLanzamiento);
+        System.out.println("Duración: " + this.duracionEnMinutos + " Min.");
+        System.out.println("Calificacion: " + nota);
+        System.out.println("**********************");
+
+        sumaDeEvaluaciones += nota;
+        totalEvaluaciones++;
+    }
     
+    void calculaMedia(){
+         System.out.println("Media de las peliculas: " + sumaDeEvaluaciones/totalEvaluaciones);
+    }
 }
--- screenmatch/src/Principal.java
+++ screenmatch/src/Principal.java
@@ -1,27 +1,13 @@
 
 public class Principal {
     public static void main(String[] args) {
-        Pelicula miPelicula= new Pelicula();
-        miPelicula.nombre="Encanto";
-        miPelicula.fechaDeLanzamiento=2022;
-        miPelicula.duracionEnMinutos=120;
-        miPelicula.incluidoEnElPlan=true;
+        Pelicula miPelicula = new Pelicula();
 
-        System.out.println("Mi pelicula es: "+ miPelicula.nombre);
-        System.out.println("Fecha de Lanzamiento: "+ miPelicula.fechaDeLanzamiento);
-        System.out.println("Duración. "+ miPelicula.duracionEnMinutos + " Min.");
-        System.out.println("**********************");
+        miPelicula.muestraFichaTecnica("Encanto", 2022, 120, 7);
+        miPelicula.muestraFichaTecnica("Matrix", 1991, 180, 10);
+        miPelicula.muestraFichaTecnica("Toy Story", 2000, 120, 10);
+        miPelicula.calculaMedia();
 
-        Pelicula otraPelicula= new Pelicula();
-        otraPelicula.nombre="Matrix";
-        otraPelicula.fechaDeLanzamiento=1999;
-        otraPelicula.duracionEnMinutos=180;
-        otraPelicula.incluidoEnElPlan=true;
-
-        System.out.println("Mi pelicula es: "+ otraPelicula.nombre);
-        System.out.println("Fecha de Lanzamiento: "+ otraPelicula.fechaDeLanzamiento);
-        System.out.println("Duración. "+ otraPelicula.duracionEnMinutos + " Min.");
-        System.out.println("**********************");
-    }    
+    }
 
 }
