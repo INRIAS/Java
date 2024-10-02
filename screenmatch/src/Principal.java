d9c06373ca7cf280d4658673c4c4ee026fb41dbc
INRIAS <inrialva@gmail.com>
2024-10-01 22:46:46 -0500

    Correcion IDE

--- screenmatch/src/Principal.java
+++ screenmatch/src/Principal.java
@@ -1,12 +1,16 @@
+import com.aluracursos.screenmatch.modelos.Pelicula;
 
 public class Principal {
     public static void main(String[] args) {
         Pelicula miPelicula = new Pelicula();
 
         miPelicula.muestraFichaTecnica("Encanto", 2022, 120, 7);
-        miPelicula.muestraFichaTecnica("Matrix", 1991, 180, 10);
-        miPelicula.muestraFichaTecnica("Toy Story", 2000, 120, 10);
+        miPelicula.muestraFichaTecnica("Encanto", 2022, 180, 10);
+        miPelicula.muestraFichaTecnica("Encanto", 2022, 120, 10);
+        System.out.println("Suma de Calificaciones: " + miPelicula.getSumaDeEvaluaciones());
         miPelicula.calculaMedia();
+        System.out.println("Total de Evaluaciones: " + miPelicula.getTotalEvaluaciones());
+
 
     }
 
