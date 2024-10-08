0287c7b72a2cdbe67724370682671834bc9ea320
INRIAS <inrialva@gmail.com>
2024-10-07 22:21:25 -0500

    Finish POO

--- screenmatch/src/Principal.java
+++ screenmatch/src/Principal.java
@@ -1,21 +1,26 @@
 import com.aluracursos.screenmatch.calculos.CalculadoraDeTiempo;
+import com.aluracursos.screenmatch.calculos.FiltroRecomendacion;
+import com.aluracursos.screenmatch.modelos.Episodio;
 import com.aluracursos.screenmatch.modelos.Pelicula;
 import com.aluracursos.screenmatch.modelos.Serie;
 
 public class Principal {
     public static void main(String[] args) {
+        System.out.println("****************************");
         Pelicula miPelicula = new Pelicula();
         miPelicula.setNombre("Encanto");
         miPelicula.setFechaDeLanzamiento(2022);
         miPelicula.setDuracionEnMinutos(120);
         miPelicula.setIncluidoEnElPlan(true);
 
+        System.out.println("****************************");
         miPelicula.muestraFichaTecnica();
         miPelicula.evalua(10);
         miPelicula.evalua(10);
         miPelicula.evalua(7.8);
         System.out.println("Média de evaluaciones de la película: " + miPelicula.calculaMedia());
 
+        System.out.println("****************************");
         Serie casaDragon = new Serie();
         casaDragon.setNombre("Casa de Dragón");
         casaDragon.setFechaDeLanzamiento(2022);
@@ -25,16 +30,30 @@ public class Principal {
         casaDragon.muestraFichaTecnica();
         System.out.println(casaDragon.getDuracionEnMinutos());
 
+        System.out.println("****************************");
         Pelicula otraPelicula = new Pelicula();
         otraPelicula.setNombre("Matrix");
         otraPelicula.setFechaDeLanzamiento(1998);
         otraPelicula.setDuracionEnMinutos(180);
 
+        System.out.println("****************************");
         CalculadoraDeTiempo calculadora = new CalculadoraDeTiempo();
         calculadora.incluye(miPelicula);
         calculadora.incluye(casaDragon);
         calculadora.incluye(otraPelicula);
         System.out.println("Duración total estimada: " + calculadora.getTiempoTotal() + " Min.");
+        
+        System.out.println("****************************");
+        FiltroRecomendacion filtroRecomendacion = new FiltroRecomendacion();
+        filtroRecomendacion.filtrar(miPelicula);
+        
+        System.out.println("****************************");
+        Episodio episodio = new Episodio();
+        episodio.setNombre("Targeryan");
+        episodio.setNumero(1);
+        episodio.setSerie(casaDragon);
+        episodio.setTotalVisualizaciones(50);
 
+        filtroRecomendacion.filtrar(episodio);
     }
 }
