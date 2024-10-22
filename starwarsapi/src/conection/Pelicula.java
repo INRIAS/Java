c06725dbb678c5ea2302bc1a30c33106a07fa994
INRIAS <inrialva@gmail.com>
2024-10-21 23:08:55 -0500

    Exercise StarWars

---
+++ starwarsapi/src/conection/Pelicula.java
@@ -0,0 +1,8 @@
+package conection;
+public record Pelicula(String title,
+        int episode_id,
+        String opening_crawl,
+        String director,
+        String producer,
+        String release_date) {
+}
